445383d3d9378d12ee949aeff06a5ff8dc5cdcf3
douglas <douglasmartinssantos7@gmail.com>
2026-04-06 17:37:32 -0300

    test: add @mock tag and enforce mocked assertions and fix what sonar pointed out.

--- tests/final_challenge.spec.js
+++ tests/final_challenge.spec.js
@@ -2,7 +2,7 @@ import { test, expect} from '../fixtures/pokeFixture.js'
 import { PokeAppPage} from '../pages/PokeAppPage.js'
 import { PokemonService } from '../services/PokemonService.js'
 
-test.describe('Final Challenge: UI + API + environment Mocks', () =>  {
+test.describe('Final Challenge: UI + API + environment Mocks @mock', () =>  {
 
     test('Should validate Charizard data across layers', async ({ page, request, smartMock}) => {
         const pokePage = new PokeAppPage(page)
@@ -11,12 +11,8 @@ test.describe('Final Challenge: UI + API + environment Mocks', () =>  {
         await page.goto('/')
         await pokePage.searchAndOpenDetails('charizard')
 
-        const idDev = process.env.BASE_URL?.includes('dev')
-
-        if(isDev) {
-            await expect(pokePage.cardTitle).toHaveText('mocked-charizard')
-            await expect(pokePage.weightText).toHaveText('9999')
-        }
+        await expect(pokePage.cardTitle).toHaveText('mocked-charizard')
+        await expect(pokePage.weightText).toHaveText('9999')
 
         const response = await pokeApi.getPokemon('charizard')
         expect(response.status()).toBe(200)
